21a9832af86f4e62c854de0ffb4207ac8b08f968
gaurav <gaurav.saraswat_cs21@gla.ac.in>
2024-04-25 20:16:08 +0530

    some changes

--- clone/src/App.jsx
+++ clone/src/App.jsx
@@ -1,12 +1,45 @@
 import Login from './login.jsx';
 import './App.css';
- 
+import React, { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './spotify.jsx';
+import SpotifyWebApi from  "spotify-web-api-js"; // this is a package that we installed to interact with spotify 
+import Player from './Player.jsx';
+
+
+const spotify = new SpotifyWebApi(); // this is an instance of the spotify web api
 
 function App() {
+
+  const [token, setToken] = useState(null)
+
+  // Run code based on a given condition 
+  useEffect(() => {
+    const hash = getTokenFromUrl();// we give all authentication work to spotify and asked for the token
+    // console.log('I HAVE A TOKEN >>>', token); // it will show the token in the console, which shouldn't be shown to the user as it is a security risk.
+    window.location.hash = ""; // this will remove the token from the url
+
+    const _token = hash.access_token; // this will get the access token from the response
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token); // this will give the access token to the spotify instance
+      spotify.getMe().then(user => {
+        console.log('person: ', user);
+      })
+    }
+
+
+  }, [])
  
   return (
     <div className='app'>
-       <Login/>
+      {
+        token ? (
+          <Player/>
+        ) : (
+          <Login/>
+        )
+      }
     </div>
   )
 }
--- clone/src/spotify.jsx
+++ clone/src/spotify.jsx
@@ -1,14 +1,13 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-// Replace with your app's client ID, redirect URI and desired scopes
 
+// Replace with your app's client ID, redirect URI and desired scopes
  const clientId = "06a195ae6dfc4af0bafee23fe9b944b0";
  
 // for getting the client id i firsst made the Spotify Developer account. and then Create an App after that i get this id which is basically shown in the url
-
-
-
 const redirectUri = "http://localhost:5174/";
+
+
 
 
 const scopes = [
@@ -20,5 +19,18 @@ const scopes = [
 ];
 // In a real application, you might use these scopes when working with the Spotify API to request specific permissions or access certain features of the user's Spotify account. 
 
-export const loginUrl=`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join( "%20")}&response_type=token&show_dialog=true`;
+// this will get the access token from the response
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      // #accessToken=mysupersecretkey&name=sonny&abc
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+} 
+
+export const loginUrl=`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
 //  here token is just like a string which represents your authentication
